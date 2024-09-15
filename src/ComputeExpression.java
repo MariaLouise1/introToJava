3a49dc0400bc0f4b5fc081774bc629f8df16f135
Henry Thompson <marialouise1216@gmail.com>
2024-09-15 18:37:44 +0100

    Compute Expressions

--- src/ComputeExpression.java
+++ src/ComputeExpression.java
@@ -2,5 +2,8 @@ public class ComputeExpression {
     public static void main(String[] args) {
         System.out.print("(10.5 + 2 * 3) / (45 - 3.5) = ");
         System.out.println((10.5 + 2 * 3) / (45 - 3.5));
+
+        System.out.print("(7.5 * 6.5) - (4.5 * 3) / (47.5 * 5.5) = ");
+        System.out.print((7.5 * 6.5) - (4.5 * 3) / (47.5 * 5.5));
     }
 }
