3b4e10cdf779876451454b1aaeac85efd6c8795b
Henry Thompson <marialouise1216@gmail.com>
2024-09-15 18:08:59 +0100

    Print Table

---
+++ src/PrintTable.java
@@ -0,0 +1,9 @@
+public class PrintTable {
+    public static void main(String[] args) {
+        System.out.printf("aa^2a^3a^4%n");
+        System.out.printf("1 1  1  1%n");
+        System.out.printf("2 4  8  18%n");
+        System.out.printf("3 9  27 81%n");
+        System.out.printf("4 16 64 256%n");
+    }
+}
